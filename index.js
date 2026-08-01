8690b531331c5b3859912afec5f3a55a6a0fe087
Arun Kanojiya <arunkanojiya123@gmail.com>
2026-08-01 21:34:01 +0530

    secret

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log("Hello Arun");
 console.log("Hello git");
 
+console.log("Secret:", process.env.NAME);
+
 console.log("Add login feature");
 console.log("Add signin feature");
 console.log("Add dark mode");
-
 console.log("Form submitted");
 console.log("Blog added");
